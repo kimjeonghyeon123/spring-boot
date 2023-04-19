ee41fe9be9bd854bde861ca6a00b626716d5d162
kimjeonghyeon123 <gojhkim123456@gmail.com>
2023-04-19 17:43:06 +0900

    5-2 완료

--- core/src/main/java/com/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/com/hello/core/order/OrderServiceImpl.java
@@ -17,6 +17,7 @@ public class OrderServiceImpl implements OrderService{
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
+
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
