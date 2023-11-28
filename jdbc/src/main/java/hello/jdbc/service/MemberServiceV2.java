e693fbc64dc563c773884249606cfce9cfc222b4
kimjeonghyeon <gojhkim123456@gmail.com>
2023-11-28 18:36:41 +0900

    추가

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,61 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/*
+트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection con = dataSource.getConnection();
+        try {
+            con.setAutoCommit(false);
+            //비즈니스 로직
+            Member fromMember = memberRepository.findById(fromId);
+            Member toMember = memberRepository.findById(toId);
+
+            memberRepository.update(fromId, fromMember.getMoney() - money);
+            validation(toMember);
+            memberRepository.update(toId, toMember.getMoney() + money);
+
+            con.commit();
+        } catch (Exception e) {
+            con.rollback();
+        } finally {
+            if (con != null) {
+                release(con);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+
+    private void release(Connection con) {
+        try {
+            con.setAutoCommit(true);
+            con.close();
+        } catch (Exception e) {
+            log.info("error", e);
+        }
+    }
+
+}
